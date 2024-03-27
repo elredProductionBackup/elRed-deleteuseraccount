21ea61b1385a6f592501fb7043483fd1bbd2b664
Shahrukh <shahrukh1494@gmail.com>
2024-03-27 16:53:41 +0530

    changed border radius of shimmer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className='header_elred'>
             <img src={logo} alt="" onClick={() => redirectToHome()} className={headerLoader ? "d-none" : ""}
                 onLoad={() => setHeaderLoader(false)} />
-            <Skeleton height={20} width={72} borderRadius={6} baseColor={"#D6DAE5"} 
+            <Skeleton height={20} width={72} borderRadius={4} baseColor={"#D6DAE5"} 
                 className={headerLoader ? "header-elred-logo-shimmer" : "d-none"} />
         </div>
     )
