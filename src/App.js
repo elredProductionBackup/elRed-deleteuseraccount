6c8b3cb59a632b75867bb31aa69d08a6aba0397a
Shahrukh <shahrukh1494@gmail.com>
2024-03-27 16:49:43 +0530

    added shimmer loader for header logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { toastConfig } from 'react-simple-toasts';
 import 'react-simple-toasts/dist/theme/dark.css'; // choose your theme
+import 'react-loading-skeleton/dist/skeleton.css'
 toastConfig({ theme: 'dark' });
 toastConfig({ maxVisibleToasts: 1 });
 
