049187dc8f18ff311bf102b2730ba159d37e0656
Shahrukh <shahrukh1494@gmail.com>
2024-05-28 15:58:24 +0530

    removed comments and unused countryData file

--- src/components/InputCountry/InputCountry.jsx
+++ src/components/InputCountry/InputCountry.jsx
@@ -1,28 +1,15 @@
 import { useState } from 'react';
 import './InputCountry.scss';
-// import { countryData } from './countryData';
 import { countryCodesData } from "../SearchableCountryCodes/countryCodesData";
 import SearchableCountryCodes from "../SearchableCountryCodes/SearchableCountryCodes";
 
 function InputCountry({number, setNumber,phoneError, setPhoneError, countryPrefix, setCountryPrefix}) {
     const [selectedCountry, setSelectedCountry] = useState(countryCodesData[4]);
-    // const [phoneNumber, setPhoneNumber] = useState('');
-    // const [isValid, setIsValid] = useState(true);
-
-    // const handleCountryChange = selectedOption => {
-    //     setCountryPrefix(selectedOption.countryCode)
-    //     setSelectedCountry(selectedOption);
-    //     // setPhoneNumber('');
-    //     setNumber('');
-    //     // setIsValid(true);
-    //     setPhoneError(false)
-    // };
 
     const handlePhoneNumberChange = (event) => {
         let inputNumber = event.target.value.replace(/\s/g, '');
         inputNumber = inputNumber.replace(/\D/g, '');
         inputNumber = inputNumber.slice(0, selectedCountry.maxDigits);
-        // setPhoneNumber(inputNumber);
         setNumber(inputNumber);
         validatePhoneNumber(inputNumber, selectedCountry.maxDigits);
     };
@@ -30,20 +17,6 @@ function InputCountry({number, setNumber,phoneError, setPhoneError, countryPrefi
     const validatePhoneNumber = (number, max) => {
         setPhoneError(number.length !== max || number[0] === "0");
     };
-
-    // const formatOptionLabel = ({ label, flag, name, countryCode }, { context }) => {
-    //     return context === "value" ? (
-    //         <div style={{ display: "flex", alignItems: "center", justifyContent:"center" }}>
-    //             {countryCode}
-    //         </div>
-    //     ) : (
-    //         <div style={{ display: "flex", alignItems: "center" }}>
-    //             <span style={{ minWidth: 35 }}>{countryCode}</span>
-    //             <img src={flag} alt={label} style={{ width: 24, marginRight: 10 }} />
-    //             {name}
-    //         </div>
-    //     );
-    // };
 
     const numberPlaceholderMapper = (selectedCountry) => {
         let zeroes = "";
@@ -56,14 +29,6 @@ function InputCountry({number, setNumber,phoneError, setPhoneError, countryPrefi
     return (
         <div className="">
             <div className='country_select'>
-                {/* <Select
-                    value={selectedCountry}
-                    onChange={handleCountryChange}
-                    options={countryData}
-                    formatOptionLabel={formatOptionLabel}
-                    getOptionLabel={option => option.name}
-                    isSearchable={false}
-                /> */}
                 <SearchableCountryCodes countryCodesData={countryCodesData} setNumber={setNumber} setPhoneError={setPhoneError}
                     selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} setCountryPrefix={setCountryPrefix} 
                     validatePhoneNumber={validatePhoneNumber} number={number} />
@@ -71,7 +36,6 @@ function InputCountry({number, setNumber,phoneError, setPhoneError, countryPrefi
                     type="text"
                     value={number}
                     onChange={handlePhoneNumberChange}
-                    // maxLength={selectedCountry.maxDigits}
                     placeholder={numberPlaceholderMapper(selectedCountry)}
                     className="country_select_input_field"
                     inputMode='numeric'
@@ -79,7 +43,6 @@ function InputCountry({number, setNumber,phoneError, setPhoneError, countryPrefi
             </div>
             {phoneError && number.length > 0 && (
                 <div className='error_msg'>
-                    {/* Invalid phone number - enter {selectedCountry.maxDigits} digits */}
                     {number?.length === selectedCountry?.maxDigits || number[0] === "0" ? "Invalid phone number" :
                     `Invalid phone number - enter ${selectedCountry?.maxDigits} digits`}
                 </div>
