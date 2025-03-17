e0337daf8eabce5dabe574d94dfcf3a158227874
JasmeenElRed <jasmeen@elred.io>
2025-03-17 11:23:06 +0530

    changes in country code

--- src/components/SearchableCountryCodes/countryCodesData.js
+++ src/components/SearchableCountryCodes/countryCodesData.js
@@ -13,105 +13,105 @@ import usa from '../../assets/flags/us-flag.webp';
 import southAfrica from "../../assets/flags/sf-flag.webp";
 
 export const countryCodesData =[
-    {
-      id: '0',
-      label: 'AU',
-      countryName: 'Australia',
-      maxDigits: 9,
-      countryCode: '+61',
-      countryFlagIcon: australia
-    },
+    // {
+    //   id: '0',
+    //   label: 'AU',
+    //   countryName: 'Australia',
+    //   maxDigits: 9,
+    //   countryCode: '+61',
+    //   countryFlagIcon: australia
+    // },
     
+    // {
+    //   id: '1',
+    //   label: 'Bahrain',
+    //   countryName: 'Bahrain',
+    //   maxDigits: 8,
+    //   countryCode: '+973',
+    //   countryFlagIcon: bahrain
+    // },
     {
       id: '1',
-      label: 'Bahrain',
-      countryName: 'Bahrain',
-      maxDigits: 8,
-      countryCode: '+973',
-      countryFlagIcon: bahrain
-    },
-    {
-      id: '2',
       label: 'Canada',
       countryName: 'Canada',
       maxDigits: 10,
       countryCode: '+1',
       countryFlagIcon: canada
     },
+    // {
+    //   id: '3',
+    //   label: 'France',
+    //   countryName: 'France',
+    //   maxDigits: 9,
+    //   countryCode: '+33',
+    //   countryFlagIcon: france
+    // },
     {
-      id: '3',
-      label: 'France',
-      countryName: 'France',
-      maxDigits: 9,
-      countryCode: '+33',
-      countryFlagIcon: france
-    },
-    {
-      id: '4',
+      id: '2',
       label: 'IN',
       countryName: 'India',
       maxDigits: 10,
       countryCode: '+91',
       countryFlagIcon: india
     },
+    // {
+    //   id: '5',
+    //   label: 'Laos',
+    //   countryName: "Lao People's Democratic Republic",
+    //   maxDigits: 8,
+    //   countryCode: '+856',
+    //   countryFlagIcon: laos
+    // },
+    // {
+    //   id: '6',
+    //   label: 'Malaysia',
+    //   countryName: 'Malaysia',
+    //   maxDigits: 10,
+    //   countryCode: '+60',
+    //   countryFlagIcon: malaysia
+    // },
+    // {
+    //   id: '7',
+    //   label: 'Poland',
+    //   countryName: 'Poland',
+    //   maxDigits: 9,
+    //   countryCode: '+48',
+    //   countryFlagIcon: poland
+    // },
+    // {
+    //   id: '8',
+    //   label: 'Portugal',
+    //   countryName: 'Portugal',
+    //   maxDigits: 9,
+    //   countryCode: '+351',
+    //   countryFlagIcon: portugal
+    // },
+    // {
+    //   id: '9',
+    //   label: 'Qatar',
+    //   countryName: 'Qatar',
+    //   maxDigits: 8,
+    //   countryCode: '+974',
+    //   countryFlagIcon: qatar
+    // },
+    // {
+    //   id: '10',
+    //   label: 'South Africa',
+    //   countryName: 'South Africa',
+    //   maxDigits: 9,
+    //   countryCode: '+27',
+    //   countryFlagIcon: southAfrica
+    // },
+    // {
+    //   id: '11',
+    //   label: 'United Kingdom',
+    //   countryName: 'United Kingdom',
+    //   maxDigits: 10,
+    //   countryCode: '+44',
+    //   countryFlagIcon: uk
+    // },
     {
-      id: '5',
-      label: 'Laos',
-      countryName: "Lao People's Democratic Republic",
-      maxDigits: 8,
-      countryCode: '+856',
-      countryFlagIcon: laos
-    },
-    {
-      id: '6',
-      label: 'Malaysia',
-      countryName: 'Malaysia',
-      maxDigits: 10,
-      countryCode: '+60',
-      countryFlagIcon: malaysia
-    },
-    {
-      id: '7',
-      label: 'Poland',
-      countryName: 'Poland',
-      maxDigits: 9,
-      countryCode: '+48',
-      countryFlagIcon: poland
-    },
-    {
-      id: '8',
-      label: 'Portugal',
-      countryName: 'Portugal',
-      maxDigits: 9,
-      countryCode: '+351',
-      countryFlagIcon: portugal
-    },
-    {
-      id: '9',
-      label: 'Qatar',
-      countryName: 'Qatar',
-      maxDigits: 8,
-      countryCode: '+974',
-      countryFlagIcon: qatar
-    },
-    {
-      id: '10',
-      label: 'South Africa',
-      countryName: 'South Africa',
-      maxDigits: 9,
-      countryCode: '+27',
-      countryFlagIcon: southAfrica
-    },
-    {
-      id: '11',
-      label: 'United Kingdom',
-      countryName: 'United Kingdom',
-      maxDigits: 10,
-      countryCode: '+44',
-      countryFlagIcon: uk
-    },
-    {
-      id: '12',
+      id: '3',
       label: 'United States of America',
       countryName: 'United States of America',
       maxDigits: 10,
