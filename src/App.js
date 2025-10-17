a10a1a0dd011ea43cb492c10ab595cc615085b1c
elredProductionBackup <productionbackup@elred.io>
2025-10-17 10:41:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { toastConfig } from 'react-simple-toasts';
-import 'react-simple-toasts/dist/theme/light.css'; // choose your theme
+import 'react-simple-toasts/dist/theme/light.css'; 
 import 'react-loading-skeleton/dist/skeleton.css'
 import axios from 'axios';
 toastConfig({ theme: 'light', maxVisibleToasts: 1, className: "react-simple-toast" });
